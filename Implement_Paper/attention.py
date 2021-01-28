a6d66f2493a7a6ac3be81085c7fb7b01cdcf7516
JminJ <jminju254@gmail.com>
2021-01-28 11:05:34 +0900

    add encoder and decoder

--- Implement_Paper/attention.py
+++ Implement_Paper/attention.py
@@ -4,25 +4,77 @@ import torch.nn as nn
 class attention(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, dropout_p):
         
-        self.input_size = input_size
+        self.input_size = input_size # input_size == embededing size
         self.output_size = output_size
         self.hidden_size = hidden_size
         self.dropout_p = dropout_p
+        self.n_layers = n_layers
 
-        self.lstm = nn.LSTM(
+        self.en_output_list = []
+
+        self.en_lstm = nn.LSTM(
             input_size = input_size,
             hidden_size = hidden_size,
             batch_first = True,
             return_state = True,
+            n_layers = self.n_layers,
+            dropout = dropout_p
+        )
+        self.de_lstm = nn.LSTM(
+            input_size = input_size,
+            hidden_size = hidden_size,
+            batch_first = True,
+            return_state = True,
+            n_layers = self.n_layers,
             dropout = dropout_p
         )
     
     def forward(self, x, h_pre):
         pass
 
-    def encoder(self, data):
-        for t in range():
-            pass
+class Encoder(nn.Module):
+    def __init__(self, input_size, hidden_size, dropout_p, n_layers = 2):
+        self.input_size = input_size # input_size == embededing size
+        self.hidden_size = hidden_size
+        self.dropout_p = dropout_p
+        self.n_layers = n_layers
 
+        self.lstm = nn.LSTM(
+            input_size = input_size,
+            output_size = hidden_size,
+            batch_first = True,
+            return_state = True,
+            n_layers = n_layers,
+            dropout = dropout_p
+        )
+
+    def forward(self, data):
+        output, (hidden, cell) = self.lstm(data)
+        self.en_output_list.append(output) 
+
+        return hidden, cell
+
+class Decoder(nn.Module):
+    def __init__(self, hidden_dize, output_size, dropout_p,n_layers = 2):
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.n_layers = n_layers
+
+        self.lstm = nn.LSTM(
+            input_size = hidden_size,
+            output_size = output_size,
+            dropout = dropout_p,
+            batch_first = True,
+            return_state = True,
+            n_layers = n_layers
+        )
+
+        self.softmax = nn.Softmax()  
+        
+
+    def forward(self, input, hidden, cell):
+        output, (hidden, cell) = self.lstm(input, (hidden, cell))
+        
         
         
