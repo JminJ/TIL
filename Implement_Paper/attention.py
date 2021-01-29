ebd7b4c626eafaeea1bce39d1c58805fc0e6fc2c
JminJ <jminju254@gmail.com>
2021-01-29 23:43:25 +0900

    add attention code(~ing code)

--- Implement_Paper/attention.py
+++ Implement_Paper/attention.py
@@ -2,42 +2,31 @@ import torch
 import torch.nn as nn
 
 class attention(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, dropout_p):
-        
+    def __init__(self, Encoder, Decoder, input_size, hidden_size, output_size, dropout_p, n_layers = 2):
+        super().__init__()
         self.input_size = input_size # input_size == embededing size
         self.output_size = output_size
         self.hidden_size = hidden_size
         self.dropout_p = dropout_p
         self.n_layers = n_layers
 
-        self.en_output_list = []
+        self.encoder = Encoder(self.input_size, self.hidden_size, self.dropout_p, self.n_layers)
+        self.decoder = Decoder(self.hidden_size, self.output_size, self.dropout_p, self.n_layers)
 
-        self.en_lstm = nn.LSTM(
-            input_size = input_size,
-            hidden_size = hidden_size,
-            batch_first = True,
-            return_state = True,
-            n_layers = self.n_layers,
-            dropout = dropout_p
-        )
-        self.de_lstm = nn.LSTM(
-            input_size = input_size,
-            hidden_size = hidden_size,
-            batch_first = True,
-            return_state = True,
-            n_layers = self.n_layers,
-            dropout = dropout_p
-        )
-    
-    def forward(self, x, h_pre):
-        pass
+        self.encoder_hidden = self.encoder.return_output_list()
+
+    def forward(self, data):
+        
 
 class Encoder(nn.Module):
     def __init__(self, input_size, hidden_size, dropout_p, n_layers = 2):
+        super().__init__()
         self.input_size = input_size # input_size == embededing size
         self.hidden_size = hidden_size
         self.dropout_p = dropout_p
         self.n_layers = n_layers
+
+        self.en_output_list = []
 
         self.lstm = nn.LSTM(
             input_size = input_size,
@@ -54,8 +43,12 @@ class Encoder(nn.Module):
 
         return hidden, cell
 
+    def return_output_list(self):
+        return self.en_output_list
+
 class Decoder(nn.Module):
-    def __init__(self, hidden_dize, output_size, dropout_p,n_layers = 2):
+    def __init__(self, hidden_dize, output_size, dropout_p, n_layers = 2):
+        super().__init__()
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.dropout_p = dropout_p
@@ -76,5 +69,4 @@ class Decoder(nn.Module):
     def forward(self, input, hidden, cell):
         output, (hidden, cell) = self.lstm(input, (hidden, cell))
         
-        
-        
+        return output
